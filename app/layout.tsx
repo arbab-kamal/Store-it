1c8fc3dea1c28a1b07c26b66707bed85ef4a4995
wingie125 <arbab.vns123@gmail.com>
2024-11-09 09:21:53 +0530

    final

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
+import { Poppins } from 'next/font/google'
 
 import "./globals.css";
 
 const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-poppins",
-});
+    subsets: ['latin'],
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+    variable: '--font-poppins',
+})
 
 export const metadata: Metadata = {
-  title: "StoreIT",
+  title: "StoreIt",
   description: "StoreIt - The only storage solution you need.",
 };
 
@@ -21,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} font-poppins antialiased`}>
+      <body
+        className={`${poppins.variable} font-poppins antialiased`}
+      >
         {children}
       </body>
     </html>
